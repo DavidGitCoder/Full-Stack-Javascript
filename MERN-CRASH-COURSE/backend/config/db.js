d5c4f02a9a5f4f4701d65012fd37edb92fc7de64
DavidGitCoder <github@solutionzmail.com>
2025-10-08 10:01:45 +0200

    adding routes and controllers

--- MERN-CRASH-COURSE/backend/config/db.js
+++ MERN-CRASH-COURSE/backend/config/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-//loads our .env file
-dotenv.config();
-console.log(process.env.MONGO_URI);
 
 export const connectDB = async () => {
   try {
+    // console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error connecting to DB: ${error}`);
-    process.exit(1); //1 code means exit with failure
+    //will kill any ongoing taks to be careful!!
+    process.exit(1); //1 code means exit with failure, 0 means success
   }
 };
--- MERN-CRASH-COURSE/backend/server.js
+++ MERN-CRASH-COURSE/backend/server.js
@@ -1,12 +1,36 @@
 import express from "express";
+import { connectDB } from "./config/db.js"; // ./ = relative path (same folder)
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/product.route.js";
+
+//loads our .env file
+dotenv.config();
 
 const app = express();
-//routes
-app.get("/products", (req, res) => {
-  res.send("Server is ready 123");
+const PORT = process.env.PORT;
+
+app.use(express.json()); //allows us to accept JSON data in the request's body
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile("../index.html");
 });
-const server = app.listen(5000, "127.0.0.1", () => {
+
+// use Postman desktop application to test without an UI
+
+const server = app.listen(PORT, () => {
   const { address, port } = server.address();
   console.log(`server started at ${address}:${port}, you're all set buddy`);
-  //console.log(server.address());
+  //  connectDB();
+  connectDB().then(() => {
+    console.log(`DB connected sucessfully`);
+  });
 });
+//test my IP
+// const getIP = async () => {
+//   const res = await fetch("https://api.ipify.org?format=json");
+//   const data = await res.json();
+//   console.log("my IP is: " + data.ip);
+// };
+// getIP();
